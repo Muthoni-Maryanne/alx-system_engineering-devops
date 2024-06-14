bfa07efd3fe24ec57cb7f7cd89c6b92fc2e221bd
Maryanne Muthoni <muthonimaryannewangare@gmail.com>
2024-06-14 12:24:26 +0300

    0. Simply matching School
    Requirements:
    The regular expression must match School
    Using the project instructions, create a Ruby script that accepts one argument and pass it to a regular expression matching method

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
-# A regular expression that is simply matching School
+# create a Ruby script that accepts one argument and
+#+pass it to a regular expression matching method
+# The regular expression must match School
 puts ARGV[0].scan(/School/).join
